be3b17bacc0b2c50ad7eb421969eac53628725e0
Ali <almutawakel.ali@gmail.com>
2017-01-30 15:37:53 -0700

    Created structure and finished home page

---
+++ header.php
@@ -0,0 +1,78 @@
+<html>
+
+<head>
+
+    <!--Jquery-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+    <!--Animate CSS-->
+    <link rel="stylesheet" type="text/css" href="css/animate.css">
+
+    <!--Font Awesome-->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.css" rel="stylesheet">
+
+    <!--Bootstrap-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script type="text/javascript" src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <!--ROBOTO FONT-->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,500" rel="stylesheet">
+
+    <!--custom CSS-->
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+
+    <!--Custom JS-->
+    <script type="text/javascript" src="js/script.js"></script>
+
+    <!--meta-->
+    <meta name="viewport" content="width=device-width">
+
+    <title>SAS</title>
+
+</head>
+
+<body>
+
+
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <a class="navbar-brand"><img class="header-logo" src="images/logo.svg"></a>
+            </div>
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="#">home</a></li>
+                    <li><a href="#">The Executives</a></li>
+                    <li><a href="#">Events</a></li>
+                    <li><a href="#">Resources</a></li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown">Memebership<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">General Members</a></li>
+                            <li><a href="#">Become an executive</a></li>
+                        </ul>
+                    </li>
+                    <li class="dropdown">
+                        <a class="dropdown-toggle" data-toggle="dropdown">Connect<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Meet our SAS Ambassadors</a></li>
+                            <li><a href="#">Have a coffee meeting</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right header-icons">
+                    <li><a href="#"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-youtube" aria-hidden="true"></i></a></li>
+                    <li><a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <main>
---
+++ home.php
@@ -0,0 +1,70 @@
+<?php include 'header.php';?>
+
+
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+            <li data-target="#myCarousel" data-slide-to="1"></li>
+            <li data-target="#myCarousel" data-slide-to="2"></li>
+            <li data-target="#myCarousel" data-slide-to="3"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+            <div class="item active">
+                <img src="http://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/China/china-touroperators-beaut-xlarge.jpg" alt="Chania">
+            </div>
+
+            <div class="item">
+                <img src="http://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/China/china-touroperators-beaut-xlarge.jpg" alt="Chania">
+            </div>
+
+            <div class="item">
+                <img src="http://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/China/china-touroperators-beaut-xlarge.jpg" alt="Chania">
+            </div>
+
+            <div class="item">
+                <img src="http://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/China/china-touroperators-beaut-xlarge.jpg" alt="Chania">
+            </div>
+
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+
+    <div class="container">
+        <div class="col-md-3 home-title">Who We Are</div>
+        <div class="col-md-9 home-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dui quis elit porttitor sodales. Curabitur ac lorem arcu. Aliquam erat volutpat. Quisque tempus lorem a erat accumsan vulputate. Ut eget facilisis libero. Aenean id felis dictum, eleifend mauris molestie, elementum erat. Ut lectus justo, sollicitudin in elit et, mattis molestie mi. Vestibulum elementum aliquam dolor. Integer elementum nulla ut dui faucibus dapibus.</div>
+
+        <div class="col-md-3 home-title">Who We Do</div>
+        <div class="col-md-9 home-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dui quis elit porttitor sodales. Curabitur ac lorem arcu. Aliquam erat volutpat. Quisque tempus lorem a erat accumsan vulputate. Ut eget facilisis libero. Aenean id felis dictum, eleifend mauris molestie, elementum erat. Ut lectus justo, sollicitudin in elit et, mattis molestie mi. Vestibulum elementum aliquam dolor. Integer elementum nulla ut dui faucibus dapibus.</div>
+
+        <div class="col-md-3 home-title">Purposes</div>
+        <div class="col-md-9 home-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dui quis elit porttitor sodales. Curabitur ac lorem arcu. Aliquam erat volutpat. Quisque tempus lorem a erat accumsan vulputate. Ut eget facilisis libero. Aenean id felis dictum, eleifend mauris molestie, elementum erat. Ut lectus justo, sollicitudin in elit et, mattis molestie mi. Vestibulum elementum aliquam dolor. Integer elementum nulla ut dui faucibus dapibus.</div>
+
+        <div class="col-md-3 home-title">Values</div>
+        <div class="col-md-9 home-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquam dui quis elit porttitor sodales. Curabitur ac lorem arcu. Aliquam erat volutpat. Quisque tempus lorem a erat accumsan vulputate. Ut eget facilisis libero. Aenean id felis dictum, eleifend mauris molestie, elementum erat. Ut lectus justo, sollicitudin in elit et, mattis molestie mi. Vestibulum elementum aliquam dolor. Integer elementum nulla ut dui faucibus dapibus.</div>
+    </div>
+
+    <div class="home-quick-link">
+        <div class="col-md-6">
+            <h3>Check Out Our Latest Event</h3>
+            <a href="#" class="btn btn-default pink" role="button">Events</a>
+        </div>
+        <div class="col-md-6">
+            <h3>Check Out Our Resources</h3>
+            <a href="#" class="btn btn-default pink" role="button">Resources</a>
+        </div>
+    </div>
+
+
+    <?php $page = "info"; include 'footer.php';?>
