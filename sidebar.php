7862ae9740a6b7f748e09f91faec8dfbb01c017a
Daniel Kuhlwein <danielkuhlwein@me.com>
2017-01-30 17:28:55 -0700

    Pointer Cursor
    Made a couple changes to the sidebar and added CSS for dropdown menus
    so that the pointer cursor is displayed when hovering over them.

--- sidebar.php
+++ sidebar.php
@@ -2,8 +2,8 @@
     
     <h3>Upcoming Events</h3>
     
-    <a href="#">Event Title</a>
-    <p>January 22, 2017</p>
+    <h4><a href="#">Event Title</a></h4>
+    <h4><small>January 22, 2017</small></h4>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse a maximus risus, id rutrum orci. Pellentesque vel massa at magna luctus faucibus ut quis sapien.</p>
     <hr>
     
